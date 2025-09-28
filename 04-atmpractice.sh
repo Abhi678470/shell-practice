edab7d0b877dc877e21fd1ba93e94d3854eb0b07
deepthi <dipthi.kanchi@gmail.com>
2025-09-28 20:14:44 +0530

    sheel-prac

--- 04-atmpractice.sh
+++ 04-atmpractice.sh
@@ -1,6 +1,6 @@
 #!bin/bash
 echo "please enter your pin number"
-read PIN_NUMBER #HERE PIN NUMBER
+read-s PIN_NUMBER #HERE PIN NUMBER
 
-echo "PIN ENTERED IS : $PIN_NUMBER
+echo "PIN ENTERED IS" : $PIN_NUMBER
 echo "you amout is read"
