21b798b5f59e55e591a2122c0adaa0d488097f88
deepthi <dipthi.kanchi@gmail.com>
2025-10-01 18:50:34 +0530

    shell

--- 14-installation.sh
+++ 14-installation.sh
@@ -2,12 +2,13 @@
 
 USERID=$(id -u)
 
-if [ $USERID -ne 0]; then
+if [ $USERID -ne 0 ]; then
     echo "please enter the root user and execute the script"
 fi
+
 dnf install mysql -y
 
-if [$? -ne 0];then
+if [ $? -ne 0 ];then
     echo "ERROR IN INSTALLING MYSQL"
 else
     echo "installalling my sql is success"    
