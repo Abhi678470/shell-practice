057804be92234d919b9627f475e764306bcc2ef2
deepthi <dipthi.kanchi@gmail.com>
2025-10-04 15:36:17 +0530

    sql

--- 20-mysqlprac.sh
+++ 20-mysqlprac.sh
@@ -4,14 +4,16 @@ USERID =$(id -u)
 
 if [ $USERID -ne 0 ]; then
     echo "kindly move to the root user"
+    exit 1
 fi
 
 dnf install mysql -y
 
 if [ $? -ne 0 ]; then
     echo "error in installing mysql"
+    exit 1
 else
     echo "installing my sql is success"
 fi
-    
+
 
